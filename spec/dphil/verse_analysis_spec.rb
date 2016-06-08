7cbe860b13677293392e09f2a82baa8648fd980b
Tim Bellefleur <nomoon@phoebus.ca>
2016-06-07 22:33:45 -0700

    Create RSpec context for sample verses and refactor VerseAnalysis spec.

--- spec/dphil/verse_analysis_spec.rb
+++ spec/dphil/verse_analysis_spec.rb
@@ -1,25 +1,24 @@
 # frozen_string_literal: true
 require "spec_helper"
+require "support/sample_verses"
 
 describe Dphil::VerseAnalysis do
-  test_verse = "dharma kṣetre kurukṣetre samavetā yuyutsavaḥ
-                māmakāḥ pāṇḍavāś caiva kima kurvata saṃjaya"
-  test_verse_syllables = %w[dhar mak ṣet re ku ruk ṣet re sa ma
-                            ve tā yu yut sa vaḥ mā ma kāḥ pāṇ
-                            ḍa vāś cai va ki ma kur va ta saṃ
-                            ja ya]
-  test_verse_syllables_weights = "GGGGLGGGLLGGLGLGGLGGLGGLLLGLLGLL"
+  include_context "sample_verses"
 
   it "returns syllables of a string" do
-    expect(described_class.syllables(test_verse)).to eq(test_verse_syllables)
+    sample_verses.each do |v|
+      expect(described_class.syllables(v["verse"])).to eq(v["syllables"])
+    end
   end
 
-  it "returns weights of corresponding syllables" do
-    expect(described_class.syllable_weight(test_verse_syllables))
-      .to eq(test_verse_syllables_weights)
+  it "returns correct weights of corresponding syllables" do
+    sample_verses.each do |v|
+      expect(described_class.syllable_weight(v["syllables"])).to eq(v["weights"])
+    end
   end
 
   it "combines syllable and weight function into one method" do
-    expect(described_class.verse_weight(test_verse)).to eq(test_verse_syllables_weights)
+    sample = sample_verses.first
+    expect(described_class.verse_weight(sample["verse"])).to eq(sample["weights"])
   end
 end
---
+++ spec/support/sample_verses.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+RSpec.shared_context "sample_verses" do
+  let(:sample_verses) { Psych.load_file(File.join(__dir__, "sample_verses.yml")) }
+end
