f0475b14dbbc8475aa3e6af8cf90d854931c4428
Tim Bellefleur <nomoon@phoebus.ca>
2016-07-10 16:26:25 -0700

    Better use of un-frozen strings for cache and log functions.

--- lib/dphil/cache.rb
+++ lib/dphil/cache.rb
@@ -9,14 +9,8 @@ module Dphil
 
   def cache(key, params = nil)
     @cache ||= defined?(::Rails.cache) ? ::Rails.cache : ActiveSupport::Cache::MemoryStore.new(size: 16_384)
-    key = "Dphil-#{Dphil::VERSION}:cache:#{key}"
-    case params
-    when nil
-    when Enumerable
-      params.map { |p| key += ":#{Digest::SHA1.base64digest(p.to_s)}" }
-    else
-      key += Digest::SHA1.base64digest(p.to_s)
-    end
-    block_given? ? @cache.fetch(key, &Proc.new) : @cache.fetch(key)
+    full_key = String.new("Dphil-#{Dphil::VERSION}:cache:#{key}")
+    full_key << ":#{Digest::SHA1.base64digest(params.to_s)}" unless params.nil?
+    block_given? ? @cache.fetch(full_key, &Proc.new) : @cache.fetch(full_key)
   end
 end
