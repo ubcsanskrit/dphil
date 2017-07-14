a28fdfff1ea5888931c9d61f87aeeed78deac226
Tim Bellefleur <nomoon@phoebus.ca>
2017-07-14 03:28:04 -0700

    Tweak hash-versioning, even though I'll probably remove it.

--- lib/dphil/version.rb
+++ lib/dphil/version.rb
@@ -1,30 +1,22 @@
 # frozen_string_literal: true
 
-require "digest"
 require "pathname"
 
 module Dphil
-  class << self
-    private
+  GEM_ROOT = Pathname.new(File.join(__dir__, "..", "..")).realpath.freeze
+  VERSION_BASE = "0.1.0"
 
-    def gem_files
-      (Pathname.glob(File.join(GEM_ROOT, "{lib,vendor}", "**", "*.{rb,yml,xml}")) +
-        Pathname.glob(File.join(GEM_ROOT, "{Gemfile,dphil.gemspec}"))).sort
-    end
+  gem_files = (
+    Pathname.glob(File.join(GEM_ROOT, "{Gemfile,*.gemspec,Rakefile}")) +
+    Pathname.glob(File.join(GEM_ROOT, "{exe,lib,spec,vendor}", "**", "*"))
+  ).select { |file| File.file?(file) }
 
-    def gem_files_hashes
-      gem_files.map do |file|
-        "#{file.relative_path_from(GEM_ROOT)}:#{Digest::SHA1.file(file).hexdigest}"
-      end
-    end
-
-    def gem_files_hash
-      Digest::SHA1.base64digest(gem_files_hashes.join("\n")).gsub(%r{^0+|[\+\=/]+}, "")[0, 5]
-    end
+  gem_files_hashes = gem_files.map do |file|
+    "#{file.relative_path_from(GEM_ROOT)}:#{Digest::SHA1.file(file).base64digest}"
   end
 
-  GEM_ROOT = Pathname.new(File.realpath(File.join(__dir__, "..", "..")))
-  private_constant :GEM_ROOT
+  gem_files_hash = Digest::SHA1.base64digest(gem_files_hashes.unshift(VERSION_BASE).join("\n"))
+                               .gsub(%r{^0+|[\+\=/]+}, "")[0, 5]
 
-  VERSION = "0.1.0-#{gem_files_hash}"
+  VERSION = "#{VERSION_BASE}.#{gem_files_hash}"
 end
