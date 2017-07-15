f33c14a15fbd3bf1bae194ae2a7190d9828299ad
Tim Bellefleur <nomoon@phoebus.ca>
2017-07-14 22:49:44 -0700

    Revamp version hash to checksum and remove from basic version number.

--- lib/dphil/version.rb
+++ lib/dphil/version.rb
@@ -1,22 +1,30 @@
 # frozen_string_literal: true
 
 require "pathname"
+require "zlib"
 
 module Dphil
   GEM_ROOT = Pathname.new(File.join(__dir__, "..", "..")).realpath.freeze
-  VERSION_BASE = "0.1.0"
 
-  gem_files = (
-    Pathname.glob(File.join(GEM_ROOT, "{Gemfile,*.gemspec,Rakefile}")) +
-    Pathname.glob(File.join(GEM_ROOT, "{exe,lib,spec,vendor}", "**", "*"))
-  ).select { |file| File.file?(file) }
+  VERSION = "0.1.0"
 
-  gem_files_hashes = gem_files.map do |file|
-    "#{file.relative_path_from(GEM_ROOT)}:#{Digest::SHA1.file(file).base64digest}"
+  VERSION_CHECKSUM = begin
+    gem_files = (
+      Pathname.glob(File.join(GEM_ROOT, "{Gemfile,*.gemspec,Rakefile}")) +
+      Pathname.glob(File.join(GEM_ROOT, "{exe,lib,vendor}", "**", "*"))
+    ).select { |file| File.file?(file) }
+
+    checksum = gem_files.reduce(Zlib.crc32) do |memo, file|
+      file_data = File.read(file)
+                      .prepend("#{file.relative_path_from(GEM_ROOT)}\n---\n")
+      Zlib.crc32(file_data, memo)
+    end
+
+    byte_str = 3.downto(0).each_with_object(String.new(capacity: 4)) do |byte, str|
+      str << ((checksum >> (byte * 8)) & 0xFF).chr
+    end
+    [byte_str].pack("m0").gsub(/[\=\+]+/, "").freeze
   end
 
-  gem_files_hash = Digest::SHA1.base64digest(gem_files_hashes.unshift(VERSION_BASE).join("\n"))
-                               .gsub(%r{^0+|[\+\=/]+}, "")[0, 5]
-
-  VERSION = "#{VERSION_BASE}.#{gem_files_hash}"
+  VERSION_FULL = "#{VERSION}-#{VERSION_CHECKSUM}"
 end
