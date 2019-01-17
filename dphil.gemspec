98152402dc1174e32d8d9332c7677c90a652a35e
Tim Bellefleur <nomoon@phoebus.ca>
2019-01-17 15:45:53 -0800

    Updates for new Roda-based webapp

--- dphil.gemspec
+++ dphil.gemspec
@@ -19,12 +19,11 @@ Gem::Specification.new do |spec| # rubocop:disable BlockLength
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.required_ruby_version = "~> 2.4"
+  spec.required_ruby_version = ">= 2.6"
 
-  spec.add_development_dependency "bundler", "~> 1.15"
+  spec.add_development_dependency "bundler", ">= 1.15"
   spec.add_development_dependency "coveralls", "~> 0.8"
   spec.add_development_dependency "pry", "~> 0.11"
-  spec.add_development_dependency "pry-byebug", "~> 3.4"
   spec.add_development_dependency "rake", "~> 12.0"
   spec.add_development_dependency "rspec", "~> 3.8"
   spec.add_development_dependency "rubocop", "~> 0.59"
@@ -42,7 +41,6 @@ Gem::Specification.new do |spec| # rubocop:disable BlockLength
   spec.add_runtime_dependency "neatjson", "~> 0.8"
   spec.add_runtime_dependency "nokogiri", "~> 1.8"
   spec.add_runtime_dependency "oj", "~> 3.6"
-  spec.add_runtime_dependency "psych", "~> 3.0"
   spec.add_runtime_dependency "sanscript", "~> 0.10"
   spec.add_runtime_dependency "unf", "~> 0.1.4"
 end
--- lib/dphil/version.rb
+++ lib/dphil/version.rb
@@ -4,27 +4,27 @@ require "pathname"
 require "zlib"
 
 module Dphil
-  GEM_ROOT = Pathname.new(File.join(__dir__, "..", "..")).realpath.freeze
+  GEM_ROOT = Pathname.new(File.expand_path("../..", __dir__)).realpath
 
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 
-  VERSION_CHECKSUM = begin
-    gem_files = (
-      Pathname.glob(File.join(GEM_ROOT, "{Gemfile,*.gemspec,Rakefile}")) +
-      Pathname.glob(File.join(GEM_ROOT, "{exe,lib,vendor}", "**", "*"))
-    ).select { |file| File.file?(file) }
+  # VERSION_CHECKSUM = begin
+  #   gem_files = (
+  #     Pathname.glob(File.join(GEM_ROOT, "{Gemfile,*.gemspec,Rakefile}")) +
+  #     Pathname.glob(File.join(GEM_ROOT, "{bin,exe,lib}/**/*")) +
+  #     Pathname.glob(File.join(GEM_ROOT, "vendor/*"))
+  #   ).select { |file| File.file?(file) }
 
-    checksum = gem_files.reduce(Zlib.crc32) do |memo, file|
-      file_data = File.read(file)
-                      .prepend("#{file.relative_path_from(GEM_ROOT)}\n---\n")
-      Zlib.crc32(file_data, memo)
-    end
+  #   checksum = gem_files.reduce(Zlib.crc32) do |memo, file|
+  #     file_data = file.read.prepend("\n-#{file.relative_path_from(GEM_ROOT)}-\n")
+  #     Zlib.crc32(file_data, memo)
+  #   end
 
-    byte_str = 3.downto(0).each_with_object(String.new(capacity: 4)) do |byte, str|
-      str << ((checksum >> (byte * 8)) & 0xFF).chr
-    end
-    [byte_str].pack("m0").gsub(/[\=\+]+/, "").freeze
-  end
+  #   byte_str = 4.times.each_with_object(String.new(capacity: 4)) do |byte, str|
+  #     str << ((checksum >> ((3 - byte) * 8)) & 0xFF).chr
+  #   end
+  #   [byte_str].pack("m0").gsub(/[\=\+]+/, "").freeze
+  # end
 
-  VERSION_FULL = "#{VERSION}-#{VERSION_CHECKSUM}"
+  # VERSION_FULL = "#{VERSION}-#{VERSION_CHECKSUM}"
 end
